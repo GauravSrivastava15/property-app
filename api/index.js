213731927918d72ccca1e088992ec1f01c6d6544
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-01-06 22:43:40 +0530

    add deployment code

--- api/index.js
+++ api/index.js
@@ -7,9 +7,11 @@ import cookieParser from "cookie-parser";
 import { appLevelErrorHandlerMiddleware } from "./middlewares/errorHandler.js";
 import connectDB from "./config/db.js";
 import propertyRouter from './property/property.routes.js'
+import path from 'path'
 
 const app = express();
 
+const __dirname = path.resolve()
 
 app.use(cookieParser());
 app.use(express.json());
@@ -17,7 +19,11 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/property", propertyRouter)
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
 
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use(appLevelErrorHandlerMiddleware)
 
 app.listen(3000, () => {
